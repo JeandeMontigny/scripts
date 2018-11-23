c3701ae2fbbd0c9893d024ea4880d75c2feddcb3
JeandeMontigny <J.De-Montigny2@newcastle.ac.uk>
2018-11-23 15:48:31 +0000

    change legend

--- ri_chart.py
+++ ri_chart.py
@@ -25,7 +25,7 @@ def figure(tab1, tab2, tab3, tab4):
     plt.plot(tab1, label="On cells")
     plt.plot(tab2, label="Off cells")
     plt.plot(tab3, label="On-Off cells")
-    plt.plot(tab4, label="All cells")
+    plt.plot(tab4, label="Mean")
     plt.xlabel("modelling time")
     plt.ylabel("regularity index")
     plt.axis([0, len(tab1), 0, 10]) # 15, 1, 5.5
