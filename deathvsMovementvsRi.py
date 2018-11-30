63c2677619cb9a3c6589cf7e81d295079d263bc1
JeandeMontigny <J.De-Montigny2@newcastle.ac.uk>
2018-11-30 13:16:47 +0000

    colour scatter plot for movement vs death RI

---
+++ deathvsMovementvsRi.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+import sys, os, re
+import numpy as np
+import matplotlib.pyplot as plt
+
+#--------------------------------------------------------------------------#
+# parse output_file to construct tabs with average RI and death rate for each simulation parameter
+def main(output_file):
+
+    moveParam=0; deathParam=0; ri_temps=[]; death_temps=[]
+    # main tabs
+    ri_mean=[]; death=[]; move_param=[]
+    fichier=open(output_file, "r")
+    file_lines=fichier.readlines()
+    for  line in file_lines:
+        # regular expression for line parsing
+        # info order: movementParam deathParam ri0 ri1 ri2 cellDensity cellDeath migrationDistance
+        m=re.search( r'(.+) (.+) (.+) (.+) (.+) (.+) (.+) (.+)', line, re.M|re.I)
+        if m:
+            # if simulation parameter for this result is not the same as before
+            if (float(m.group(1)) != moveParam):
+                # if temps tabs are not empty, to avoid adding to main tab empty cells
+                if not len(ri_temps) == 0:
+                    # append average simulation results for this parameter to the main tab
+                    ri_mean.append(np.average(ri_temps))
+                    death.append(np.average(death_temps))
+                    move_param.append(float(m.group(1)))
+                # empty temps tab for next simulation parameter results
+                ri_temps=[]
+                death_temps=[]
+                moveParam = float(m.group(1))
+            # append this simulation results to temps tabs
+            ri_temps.append((float(m.group(3))+float(m.group(4))+float(m.group(5)))/3)
+            death_temps.append(float(m.group(7)))
+
+    # need to append the last param values to main tabs
+    ri_mean.append(np.average(ri_temps))
+    death.append(np.average(death_temps))
+    move_param.append(float(m.group(1)))
+
+    # print corresponding index for specified death value
+#    for i, value in enumerate(death):
+#        if value > 79 and value < 80:
+#            print(i)
+
+    # call figure construction
+    figure(ri_mean, death, move_param)
+
+#--------------------------------------------------------------------------#
+# simple plot of death vs RI (with std)
+def figure(ri, deathRate, move_param):
+
+    plt.figure(1)
+    plt.scatter(deathRate, move_param, c=ri, marker='s', s=64)
+    plt.colorbar().set_label('Regularity index')
+    plt.xlabel("Death rate")
+    plt.ylabel("Movement threshold")
+    plt.title("Cell death and cell migration impact on RI")
+
+    plt.show()
+
+#--------------------------------------------------------------------------#
+# check number of arguments
+if len(sys.argv)==2:
+    main(sys.argv[1])
+    print("done")
+else:
+    exit("arg error - need 1 arg: [death_RI file]")
+
