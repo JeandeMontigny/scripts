a3b28ea8c2156cd2aa24a1b4de5832439f3fe0ea
JeandeMontigny <J.De-Montigny2@newcastle.ac.uk>
2019-01-09 13:38:10 +0100

    clean and comment

--- ri_imagej.py
+++ ri_imagej.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python3
 import sys, os, re
 import numpy as np
-#import matplotlib.pyplot as plt
 
 # compute RI from imagej cell position extraction
 #--------------------------------------------------------------------------#
 def main(coord_file):
+    """Extract x,y coordinate from ImageJ cell position extraction then compute the Regularity Index (RI) corresponding to this cell population.
+    Argument: file containing X,Y coordinates of every cells, sorted as "index,x,y" format """
     cells_position=[]
     fichier=open(coord_file, "r")
     file_lines=fichier.readlines()
     for line in file_lines:
         # index,x,y
-#        m=re.search( r'[0-9]+,([0-9]+),([0-9]+)', line, re.M|re.I)
-        # with "point selection": index,Area,Mean,Min,Max,X,Y
+        # m=re.search( r'[0-9]+,([0-9]+),([0-9]+)', line, re.M|re.I)
+        # with "point selection" method: index,Area,Mean,Min,Max,X,Y
         m=re.search( r'[0-9]+,[0-9.]+,[0-9.]+,[0-9.]+,[0-9.]+,([0-9.]+),([0-9.]+)', line, re.M|re.I)
         if m :
             cells_position.append([float(m.group(1)), float(m.group(2))])
 
     shortest_dist_list=getShortestDistList(cells_position)
     ri = np.average(shortest_dist_list)/np.std(shortest_dist_list)
-    print("ri for that mosaic:", round(ri, 2))
-
+    print("ri for this mosaic:", round(ri, 2))
 
 #--------------------------------------------------------------------------#
 def getShortestDistList(coord_list):
@@ -39,7 +39,7 @@ def getShortestDistList(coord_list):
 
 #--------------------------------------------------------------------------#
 if len(sys.argv)!=2:
-    exit("arg error - need 2 arg: [cells_position_file]")
+    exit("arg error - need 1 arg: [cells_position_file]")
 else:
     main(sys.argv[1])
     print("done")
