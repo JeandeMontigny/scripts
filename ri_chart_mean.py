a16edac7ea4923eeee40f1e79169513c07324621
JeandeMontigny <J.De-Montigny2@newcastle.ac.uk>
2019-05-07 14:44:10 +0100

    add multiplot for individual simulation plot

--- ri_chart_mean.py
+++ ri_chart_mean.py
@@ -6,19 +6,21 @@ import scipy.signal as sig
 
 #--------------------------------------------------------------------------#
 def main(folder):
-    ri_mean = []; death_rate = []
+    ri_mean = []; death_rate = []; names = []
     for name in os.listdir(folder):
         if name.endswith(".txt"):
             results = read(folder+"/"+name)
             ri_mean.append(results[0])
             death_rate.append(results[1])
-#            plotSimu(results[0], name); plt.show()
+            names.append(name)
 
     simu_nb = len(ri_mean); simu_len = len(ri_mean[0])
     for ri in ri_mean:
         if simu_len != len(ri):
             print("error - simulation length is not the same for every simulation")
-            sys.exit()
+            return 0
+
+#    plotIndividualSimus(ri_mean, death_rate, names)
 
     x_axis = []
     mean_ri_mean = []; mean_ri_std = []
@@ -76,11 +78,34 @@ def figure(x, y, std, y_label):
         plt.ylim(3000, 12500)
 
 #--------------------------------------------------------------------------#
-def plotSimu(ri, name):
+def plotIndividualSimus(ri_list, death_list, name_list):
+    nb_row = int(np.sqrt(len(ri_list)))
+    nb_col = nb_row+1
+
+    plt.figure(98)
+    for i in range (0, len(ri_list)):
+        ri = ri_list[i]
+        plt.subplot(nb_row, nb_col, i+1)
+        plt.plot(range(0, len(ri)), ri)
+        plt.ylim(1, 7)
+        plt.title(name_list[i])
+        plt.subplots_adjust(left=0.05, right=0.95, bottom = 0.05, hspace=0.5)
+
     plt.figure(99)
-    plt.plot(range(0, len(ri)), ri)
-    plt.ylim(1, 7)
-    plt.title(name)
+    for i in range (0, len(ri_list)):
+        ri = ri_list[i]
+        death = death_list[i]
+        diff_ri = []
+        for j in range(10, 72):
+            diff_ri.append(ri[j]-ri[j-1])
+        diff_ri = sig.savgol_filter(diff_ri, 7, 3)
+        plt.subplot(nb_row, nb_col, i+1)
+        plt.plot(death[10:72], diff_ri, color='black')
+        plt.axhline(y=0, color='gray', linestyle='--')
+        plt.title(name_list[i])
+        plt.subplots_adjust(left=0.05, right=0.95, bottom = 0.05, hspace=0.5)
+
+    plt.show()
 
 #--------------------------------------------------------------------------#
 def figureRiVsDeath(mean_ri, mean_death):
