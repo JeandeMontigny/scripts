614d01ae3c88339a4539610333d0ec1319fd37f6
JeandeMontigny <J.De-Montigny2@newcastle.ac.uk>
2019-09-20 15:06:25 +0200

    sort data before plotting

--- surface_plot.py
+++ surface_plot.py
@@ -15,7 +15,7 @@ def main(fodlers):
             mean_area.append(temps[1])
             std_area.append(temps[2])
 
-    plot(mean_area, std_area, label)
+    plot(sortData(label, mean_area, std_area))
 
     return 1
 
@@ -26,11 +26,11 @@ def extractFolderData(folder):
     if len(list_file) == 0:
         return 0
     # extract folder corresponding developmental day. assume it won't be higher that P99
-    label = folder[-2:] if folder[-2:-1] == 'P' else folder[-3:]
+    label = folder[-1:] if folder[-2:-1] == 'P' else folder[-2:]
     for file in list_file:
         areas.append(readFile(folder+"/"+file))
 
-    return label, round(np.average(areas), 2), round(np.std(areas), 2)
+    return int(label), round(np.average(areas), 2), round(np.std(areas), 2)
 
 #--------------------------------------------------------------------------#
 def readFile(file):
@@ -41,12 +41,28 @@ def readFile(file):
 
     return float(m.group(1))/1e6 if m else 0
 
+
 #--------------------------------------------------------------------------#
-def plot(mean, std, label):
-#    x = [value for value in range(0, len(mean))]
+def sortData(label, mean, std):
+    # insertion sort
+    i = 1
+    while i < len(label):
+        j = i
+        while j > 0 and label[j-1] > label[j]:
+            label[j], label[j-1] = label[j-1], label[j]
+            mean[j], mean[j-1] = mean[j-1], mean[j]
+            std[j], std[j-1] = std[j-1], std[j]
+            j = j-1
+        i = i+1
+
+    return label, mean, std
+
+#--------------------------------------------------------------------------#
+def plot(data):
+    label = data[0]; mean = data[1]; std = data[2]
     plt.errorbar(label, mean, std)
     plt.xlabel("Postnatal day")
-    plt.ylabel("Retinal surface")
+    plt.ylabel("Retinal surface (mm$^2$)")
 
     plt.show()
 
