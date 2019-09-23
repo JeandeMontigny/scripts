8f9837960b5544dab433d9978479f75cd712f9a6
JeandeMontigny <J.De-Montigny2@newcastle.ac.uk>
2019-09-23 15:30:51 +0200

    add figures for surface, corrected rgcnb and corrected death

--- apo_figure.py
+++ apo_figure.py
@@ -13,6 +13,15 @@ def main():
     rgc_ri = [4.81, 5.12, 5.45, 5.49, 5.17, 5.28, 5.62, 6.03, 6.1]
     rgc_ri_std = [0.57, 0.61, 0.58, 0.35, 0.48, 0.5, 0.69, 0.43, 0.48]
 
+    surface = [11.36, 12.48, 13.08, 13.66, 13.9, 14.4, 14.88, 15.16, 15.05] # +, 15.67] for day 11
+    surface_std = [0.73, 2.24, 1.77, 1.83, 1.91, 1.8, 1.65, 2.4, 1.75] # +, 1.47] for day 11
+
+    corrected_rgc_nb = [76905.17, 59836.19, 50788.67, 48172.12, 47281.57, 47635.66, 47927.51, 48095.77, 48527.03]
+    corrected_rgc_std = [2705.69, 5261.80, 2497.52, 1684.67, 2145.28, 1812.71, 2644.79, 4136.08, 2696.44]
+
+    death = [0.0, 45.83, 82.58, 92.44693968212697, 99.55, 99.20, 98.49, 100.0]
+    death_std = [31.34, 30.81, 14.75, 13.12, 13.58, 15.85, 25.58, 25.74]
+
     sac_gcl_nb = [np.nan, np.nan, np.nan, 1008.46, 1098.73, 1063.77, 1109.7, 1074.22, 1087.29]
     sac_gcl_nb_std = [np.nan, np.nan, np.nan, 90.96, 121.4, 132.62, 62.03, 108.77, 109.27]
     sac_gcl_ri = [np.nan, np.nan, np.nan, 3.64, 3.93, 3.76, 3.58, 3.63, 3.53]
@@ -25,6 +34,10 @@ def main():
 
     createFigure(dev_days, rgc_nb, rgc_nb_std, "Postnatal day", "Cell density", "RGC density during development")
     createFigure(dev_days, rgc_ri, rgc_ri_std, "Postnatal day", "RI", "RGC Regularity Index during development")
+
+    createFigure(dev_days, surface, surface_std, "Postnatal day", "Retinal surface", "Retinal surface during development")
+    createFigure(dev_days, corrected_rgc_nb, corrected_rgc_std, "Postnatal day", "Estimated cell population", "Estimated RGC population during development")
+    createFigure(dev_days[:-1], death, death_std, "Postnatal day", "Cell death (%)", "RGC cumulative death during development")
 
     createFigure(dev_days, sac_gcl_nb, sac_gcl_nb_std, "Postnatal day", "Cell density", "GCL SAC density during development")
     createFigure(dev_days, sac_gcl_ri, sac_gcl_ri_std, "Postnatal day", "RI", "GCL SAC Regularity Index during development")
